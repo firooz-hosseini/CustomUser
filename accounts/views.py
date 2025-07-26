e5d2fd2e3ac2344f0d43ba662636768998897275
Firooz Hosseini <firo744@gmail.com>
2025-07-26 12:43:43 +0330

    Fix confirm view

--- accounts/views.py
+++ accounts/views.py
@@ -15,18 +15,18 @@ class SignUpView(View):
         return render(request, self.html_file, {'form': self.form})
     
     def post(self, request):
-        filled_form = MyUserCreationForm(request.POST)
-        if filled_form.is_valid():
-            mobile = filled_form.cleaned_data.get('mobile')
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            mobile = form.cleaned_data.get('mobile')
             user = CustomUser.objects.filter(mobile = mobile).exists()
             if not user :
-                code = random.randint(1000,9999)
+                code = str(random.randint(1000,9999))
                 request.session['user_registration_info'] = {
-                                'phone_number': filled_form.cleaned_data['mobile'],
-                                'password': filled_form.cleaned_data['password'],
+                                'mobile': form.cleaned_data['mobile'],
+                                'password': form.cleaned_data['password'],
                                 'code':code,
                         }
-                print(code,'confirmation')
+                print('confirmation code:', code)
                 return redirect('confirm')
 
 
@@ -38,10 +38,10 @@ class ConfirmView(View):
 
     def post(self, request):
         user_session = request.session['user_registration_info']
-        filled_form = ConfirmForm(request.POST)
-        if filled_form.is_valid():
-            verified_code = filled_form.cleaned_data['confirm_code']
-            if verified_code is user_session.get('code'):
+        form = ConfirmForm(request.POST)
+        if form.is_valid():
+            verified_code = form.cleaned_data['confirm_code']
+            if verified_code == user_session.get('code'):
                 mobile = user_session.get('mobile')
                 password = user_session.get('password')
                 CustomUser.objects.create_user(mobile = mobile, password=password)
